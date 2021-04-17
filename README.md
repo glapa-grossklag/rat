fcf380109ea6359e588de9392daa6313c391a157
Miles Glapa-Grossklag <m.glapa.grossklag@gmail.com>
2021-04-17 11:27:30 -0700

    Add command line arguments

--- README.md
+++ README.md
@@ -5,5 +5,10 @@ Flip random input bits from `stdin`, send them to `stdout`.
 # Usage
 
 ```sh
-$ echo "Hello, world." | cargo run
+cargo run -- -h
 ```
+
+The default error rate is 0.1% of bits, but can be changed using the `-e` or
+`--error` option. Note that if the error rate is less than 0, no bits will be
+flipped. Likewise, if the error rate is greater than 1, all bits will be
+fipped.
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
 mod bits;
 
+use getopts::Options;
 use std::io;
 use std::io::prelude::*;
 
-const ERROR_RATE: f64 = 0.10;
+const DEFAULT_ERROR_RATE: f64 = 0.001;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
     let mut buffer: Vec<u8> = Vec::new();
+
+    // Define arguments.
+    let mut opts = Options::new();
+    opts.optopt("e", "error", "The rate at which to flip bits.", "RATE");
+    opts.optflag("h", "help", "Print this information.");
+
+    // Match arguments.
+    let matches = match opts.parse(&args[1..]) {
+        Err(why) => panic!("{}", why.to_string()),
+        Ok(m) => m,
+    };
+
+    // -h, --help
+    if matches.opt_present("h") {
+        let usage = format!("Usage:\n    {} [options]", args[0]);
+        print!("{}", opts.usage(&usage));
+        return;
+    }
+
+    // -e, --error
+    let error_rate = match matches.opt_get_default("e", DEFAULT_ERROR_RATE) {
+        Err(why) => panic!("Cannot read option: {}", why),
+        Ok(x) => x,
+    };
+
+    // Read.
     match io::stdin().read_to_end(&mut buffer) {
         Err(why) => panic!("Cannot read: {}", why),
         Ok(_) => (),
@@ -14,9 +42,10 @@ fn main() {
 
     // Randomly flip bits.
     for byte in buffer.iter_mut() {
-        if fastrand::f64() <= ERROR_RATE {
-            let index = fastrand::u8(0..=7);
-            bits::flip(byte, index);
+        for bit in 0..8 {
+            if fastrand::f64() <= error_rate {
+                bits::flip(byte, bit);
+            }
         }
     }
 
@@ -25,4 +54,6 @@ fn main() {
         Err(why) => panic!("Cannot write: {}", why),
         Ok(_) => (),
     }
+
+    return;
 }
